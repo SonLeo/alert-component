e262c62bcf7dde034b4f3382f023b426bfef3493
Trinh Van Son <sontrinh.tp@gmail.com>
2023-06-24 22:48:03 +0700

    add file

---
+++ src/components/Alert.js
@@ -0,0 +1,9 @@
+function Alert(props) {
+    return (
+        <div class="alert alert-warning mt-4 mb-4 me-4 ms-4" role="alert">
+            Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+        </div>
+    );
+};
+
+export default Alert;
